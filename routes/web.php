266be528942997b270f396d96ea2ab6aeb74373e
DavidCutz <davidcutz25cona@gmail.com>
2023-07-05 20:40:03 -0500

    mis vistas

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,8 @@ Route::post('/import', [ExcelImportController::class, 'import']);
 Route::view('alumno','alumnos/alumnos');
 Route::view('consulta', 'consultas/Consultas');
 Route::view('clave', 'clave/clave');
+Route::view('capitulo','capitulo/capitulo');
+Route::view('partida','partida/partida');
 
 Route::view('concentrado', 'concentrado/concentrado');
 // rutas apis(controladores)
@@ -37,6 +39,9 @@ Route::apiResource('apiAlumno',AlumnosController::class);
 Route::apiResource('apiConsulta', ConsulasControlador::class);
 Route::apiResource('apiConcentrado', ConcentradoController::class);
 Route::apiResource('apiClave', ClaveController::class);
+Route::apiResource('apiCapitulo',CapituloController::class);
+Route::apiResource('apiPartida',PartidaController::class);
+
 });
 
 // rutas apis(controladores)
@@ -46,7 +51,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::view('capitulo','capitulo/capitulo');
-Route::view('partida','partida/partida');
-Route::apiResource('apiCapitulo',CapituloController::class);
-Route::apiResource('apiPartida',PartidaController::class);
+
