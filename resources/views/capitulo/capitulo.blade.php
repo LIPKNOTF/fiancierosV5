fb608f44333dc8e503f1a2b6c73f166896f615a8
DavidCutz <davidcutz25cona@gmail.com>
2023-11-14 15:49:34 -0600

    capitulo bueno

--- resources/views/capitulo/capitulo.blade.php
+++ resources/views/capitulo/capitulo.blade.php
@@ -1,115 +1,84 @@
-@extends('layouts.master')
+@extends('layouts.app')
 @section('titulo','Capitulo')
 @section('content')
 
 <div id="capitulos">
+    <div class="row">
+        <div class="col-md-12">
+            <pre></pre>
+            <div class="card header">
+                <div class="content-header">
+                    <h3>@{{prueba}}
+                        <button class="btn btn-sm" @click="mostrarModal()">
+                            <i class="fas fa-user-plus"></i>
+                        </button>
+                    </h3>
 
-    <button class="btn-modal" @click="mostrarModal()">
-        Agregar
-    </button>
+                    <!--Inicion de la tabla-->
+                    <table class="table table-bordered">
+                        <thead class="thead-dark">
+                            <th>ID</th>
+                            <th>CODIGO</th>
+                            <th>TITULO</th>
+                            <th>OPCIONES</th>
+                        </thead>
 
-    <input type="text" placeholder="Bucar (Codigo)" class="input" v-model="buscar">
+                        <tbody v-for="Capitulo in capitulos">
+                            <tr>
+                                <td>@{{Capitulo.id}}</td>
+                                <td>@{{Capitulo.codigo}}</td>
+                                <td>@{{Capitulo.titulo}}</td>
+                                <td>
+                                    <button class="btn btn-sm">
+                                        <i class="fa-regular fa-pen-to-square" @click="editarCapitulo(Capitulo.id)"></i>
+                                    </button>
+                                    <button class="btn btn-sm" @click="eliminarCapitulo(Capitulo.id)">
+                                        <i class="fa-solid fa-eraser"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
 
-    <table id="TablaCapitulo" class="tabla display nowrap" style="width:100%">
-        <thead class="fondo-negro">
-            <tr>
-                <th class="boder-inicio">ID</th>
-                <th>CODIGO</th>
-                <th>TITULO</th>
-                <th class="boder-fin">OPCIONES</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="Capitulo in filtroCapitulo">
-                <td>@{{Capitulo.id}}</td>
-                <td>@{{Capitulo.codigo}}</td>
-                <td>@{{Capitulo.titulo}}</td>
-                <td>
-                    <button class="btn-edit" title="Editar" @click="editarCapitulo(Capitulo.id)">
-                        <i class="fa-solid fa-pen"></i>
-                    </button>
-                    <button class="btn-delete" title="Eliminar" @click="eliminarCapitulo(Capitulo.id, Capitulo.titulo)">
-                        <i class="fa-solid fa-trash"></i>
-                    </button>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
 
-<!-- INICIA VENTANA MODAL -->
-<div class="modal fade" id="modalCapitulo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header" style="background-color: #96C7EB;">
-                <h5 class="modal-title" id="exampleModalLabel" v-if="agregando==true">Agregando Capitulo</h5>
-                <h5 class="modal-title" id="exampleModalLabel" v-if="agregando==false">Editando Capitulos</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-
-            <div class="modal-body">
-                <input type="text" class="form-control" placeholder="Codigo" v-model="codigo" @input="validarCodigo"><br>
-                <input type="text" class="form-control" placeholder="Titulo" v-model="titulo" @input="validarTitulo"><br>
-            </div>
-
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                <button type="button" class="btn btn-primary" @click="guardarCapitulo()" v-if="agregando==true">Guardar</button>
-                <button type="button" class="btn btn-primary" @click="actualizarCapitulo()" v-if="agregando==false">Guardar</button>
+                </div>
             </div>
         </div>
     </div>
+
+    <!-- INICIA VENTANA MODAL -->
+    <div class="modal fade" id="modalCapitulo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel" v-if="agregando==true">Agregando Capitulo</h5>
+                    <h5 class="modal-title" id="exampleModalLabel" v-if="agregando==false">Editando Capitulos</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <div class="modal-body">
+                    <input type="number" class="form-control" placeholder="Id" v-model="id"><br>
+                    <input type="text" class="form-control" placeholder="Capitulo" v-model="codigo"><br>
+                    <input type="text" class="form-control" placeholder="Titulo" v-model="titulo"><br>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+                    <button type="button" class="btn btn-primary" @click="guardarCapitulo()" v-if="agregando==true">Guardar</button>
+                    <button type="button" class="btn btn-primary" @click="actualizarCapitulo()" v-if="agregando==false">Guardar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- FIN MODAL -->
+
 </div>
-<!-- FIN MODAL -->
 
 @endsection
 
 @push('scripts')
-<script>
-    $(document).ready(function() {
-        var table = $("#TablaCapitulo");
-        if (!table.hasClass("dataTable")) {
-            table.DataTable({
-                initComplete: function() {
-                    this.api()
-                        .columns()
-                        .every(function(index) {
-                            var column = this;
-                            var header = $(column.header());
-                            if (header.hasClass("actions")) {
-                                // No hacer nada si es la columna de acciones
-                                return;
-                            }
-                        });
-                },
-                responsive: {
-                    details: {
-                        type: 'inline', // Cambiado de 'column' a 'inline'
-                        target: ':not(:last-child)' // Excluir la última columna
-                    }
-                },
-                language: {
-                    searchPlaceholder: "Buscar",
-                    search: "Buscar:",
-                    zeroRecords: "No se encontraron resultados",
-                    emptyTable: "No hay datos disponibles en la tabla",
-                    infoEmpty: "Mostrando 0 registros de un total de 0",
-                    infoFiltered: "(filtrado de un total de MAX registros)",
-                    example_info: "Se muestran 0 de 0 un total de 0",
-                    sInfo: "<span style='margin-left: 2rem;'>Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros</span>",
-                    lengthMenu: "Mostrar _MENU_",
-                    paginate: {
-                        previous: "Anterior",
-                        next: "Siguiente",
-                    },
-                },
-
-                "lengthMenu": [7, 10, 25, 50],
-                "pageLength": 7,
-            });
-        }
-    });
-</script>
 <script type="text/javascript" src="js/apis/apiCapitulo.js"></script>
 <script type="text/javascript" src="js/vue-resource.js"></script>
 @endpush
